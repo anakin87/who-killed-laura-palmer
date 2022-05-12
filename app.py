274e5e79ed304bdd075c4393c16d6cd7b1c5a976
Stefano Fiorucci <44616784+anakin87@users.noreply.github.com>
2022-05-12 21:05:15 +0200

    no cache for the moment

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def set_state_if_absent(key, value):
         st.session_state[key] = value
 
 # hash_funcs={builtins.weakref: my_hash_func}
-@st.cache(persist=True, hash_funcs={"builtins.weakref": lambda _: None}, allow_output_mutation=True)
+# @st.cache(persist=True, hash_funcs={"builtins.weakref": lambda _: None}, allow_output_mutation=True)
 def query(pipe, question, retriever_top_k=10, reader_top_k=5) -> dict:
     """Run query and get answers"""
     return (pipe.run(question, 
